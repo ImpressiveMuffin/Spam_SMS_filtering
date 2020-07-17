4c57872c3c924fefad34c4d82fe7fa4122f6aa4a
abinash-boruah <abinash.boruah09@gmail.com>
2020-07-17 10:56:15 +0530

    Add files via upload

---
+++ graph.h
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt 
+ 
+x = ['Precision','Recall','F-Score','Accuracy'] 
+y = [0.8918918918918919,0.75,0.8148148148148148, 0.9562043795620438]
+
+y1 = [0.8770491803278688,0.6079545454545454,0.7181208053691275, 0.9386861313868613]
+plt.plot(x1, y1, x2, y2) 
+plt.xlabel('x - axis') 
+plt.ylabel('y - axis')  
+plt.title('My first graph!')   
+plt.show() 
+
